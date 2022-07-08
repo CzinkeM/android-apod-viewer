02d1bc48e44b2ebd5bf93b92922c0b8d2bd07c09
CzinkeM <mczinke@protonmail.com>
2022-07-08 14:31:52 +0200

    Added model classes.

---
+++ app/src/main/java/hu/mczinke/payment_manager/models/APOD.kt
@@ -0,0 +1,28 @@
+package hu.mczinke.payment_manager.models
+
+import com.google.gson.annotations.SerializedName
+
+
+data class APOD(
+    @SerializedName("copyright")
+    val copyright: String?,
+    @SerializedName("date")
+    val date: String,
+    @SerializedName("explanation")
+    val explanation: String,
+    @SerializedName("hdurl")
+    val HDUrl: String,
+    @SerializedName("media_type")
+    val mediaType: String,
+    @SerializedName("service_version")
+    val version: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("url")
+    val url: String
+) {
+
+    override fun toString(): String {
+        return "$copyright: $title($date)"
+    }
+}
---
+++ app/src/main/java/hu/mczinke/payment_manager/models/APODImage.kt
@@ -0,0 +1,6 @@
+package hu.mczinke.payment_manager.models
+
+import android.graphics.Bitmap
+import android.media.Image
+
+data class APODImage(val image: Bitmap?)
