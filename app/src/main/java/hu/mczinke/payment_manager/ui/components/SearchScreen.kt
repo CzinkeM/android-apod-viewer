7632e137d9437c9eb62b746f1dd02bee923b8211
CzinkeM <mczinke@protonmail.com>
2022-07-27 16:27:09 +0200

    Worked on searching ux and ui.

--- app/src/main/java/hu/mczinke/payment_manager/ui/components/SearchScreen.kt
+++ app/src/main/java/hu/mczinke/payment_manager/ui/components/SearchScreen.kt
@@ -2,173 +2,209 @@ package hu.mczinke.payment_manager.ui.components
 
 import android.app.DatePickerDialog
 import android.widget.DatePicker
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import hu.mczinke.payment_manager.models.Apod
+import hu.mczinke.payment_manager.ui.theme.DimmedWhite
 import hu.mczinke.payment_manager.ui.theme.SpaceBlackVariant
 import hu.mczinke.payment_manager.ui.theme.SpacePrimaryVariant
+import hu.mczinke.payment_manager.ui.theme.SpaceSecondary
+import hu.mczinke.payment_manager.viewmodels.SearchViewModel
 import java.util.*
 
 @Composable
-fun SearchScreen() {
-    val testList = listOf("alma", "körte", "banán")
+fun SearchScreen(viewModel: SearchViewModel) {
+    val apods: List<Apod> by viewModel.apods.observeAsState(listOf(Apod.nullApod()))
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         LazyColumn() {
-            item { SearchTitle() }
-            items(items = testList) { label ->
-                Text(text = label)
+            item { SearchWidget() }
+            items(items = apods) { apod ->
+                Text(text = apod.title)
             }
         }
     }
 }
 
 @Composable
-fun SearchTitle() {
+fun SearchWidget() {
+    // TODO: Akkor igaz ha van választva dátum
+    var disabledRange: Boolean = false
+    var disabledDate: Boolean = false
+    val showButton: Boolean = false
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
         backgroundColor = SpaceBlackVariant,
-        elevation = 8.dp
+        elevation = 8.dp,
+        shape = RoundedCornerShape(16.dp)
     ) {
-        Column {
-            SpecificDatePickerWidget()
-            OrDivider()
-            DateRangePickerWidget()
-        }
-    }
-    SearchBar()
-}
-
-@Composable
-fun SpecificDatePickerWidget() {
-    Column {
-        Text(text = "Specific Date")
-        Button(
-            onClick = { /*TODO*/ },
-            colors = ButtonDefaults.outlinedButtonColors(),
-            shape = RoundedCornerShape(16.dp)
+        Column(
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("2002-02-07")
+
+            specificDatePickerWidget(disabledDate)
+            OrDivider()
+            dateRangePickerWidget(disabledRange)
+            if (showButton) {
+                SearchButton()
+            }
         }
     }
 }
 
 @Composable
-fun OrDivider() {
-    Box(
+fun specificDatePickerWidget(disable: Boolean): String {
+    val context = LocalContext.current;
+    val date = remember { mutableStateOf("") }
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-    ) {
-        Divider(
-            color = SpacePrimaryVariant,
-            thickness = 1.dp,
-            modifier = Modifier
-                .fillMaxWidth(0.4f)
-                .padding(8.dp, 0.dp)
-                .align(Alignment.CenterStart)
-        )
-        Text(text = "OR", color = SpacePrimaryVariant, modifier = Modifier.align(Alignment.Center))
-        Divider(
-            color = SpacePrimaryVariant,
-            thickness = 1.dp,
-            modifier = Modifier
-                .fillMaxWidth(0.4f)
-                .padding(8.dp, 0.dp)
-                .align(Alignment.CenterEnd)
-        )
-    }
-    /*
-    Row(modifier = Modifier.fillMaxWidth(1f).padding(0.dp,8.dp), horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically ) {
-        Divider(color = Color.Green, thickness = 1.dp, modifier = Modifier.padding(8.dp,0.dp))
-        Text(text = "OR", color = Color.Red)
-        Divider(color = Color.Red, thickness = 1.dp, modifier = Modifier.padding(8.dp,0.dp))
-        //Divider(color = Color.Gray, thickness = 1.dp)
-    }
-     */
-}
-
-@Composable
-fun DateRangePickerWidget() {
-    Column() {
-        Text(text = "Interval")
-        Button(
-            onClick = { /*TODO*/ },
-            colors = ButtonDefaults.outlinedButtonColors(),
-            shape = RoundedCornerShape(16.dp)
-        ) {
-            Text("2002-02-07")
-        }
-    }
-}
-
-@Composable
-fun SearchBar() {
-    val mContext = LocalContext.current
-
-    val mYear: Int
-    val mMonth: Int
-    val mDay: Int
-
-    val mCalendar = Calendar.getInstance()
-
-    // Fetching current year, month and day
-    mYear = mCalendar.get(Calendar.YEAR)
-    mMonth = mCalendar.get(Calendar.MONTH)
-    mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
-
-    mCalendar.time = Date()
-
-    // Declaring a string value to
-    // store date in string format
-    val mDate = remember { mutableStateOf("") }
-
-    // Declaring DatePickerDialog and setting
-    // initial values as current values (present year, month and day)
-    val mDatePickerDialog = DatePickerDialog(
-        mContext,
-        { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
-            mDate.value = "$mDayOfMonth/${mMonth + 1}/$mYear"
-        }, mYear, mMonth, mDay
-    )
-    mDatePickerDialog.datePicker.maxDate = mCalendar.time.time
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
+            .clickable {
+                //todo: Should change on the button click as well
+                Toast
+                    .makeText(context, "hello", Toast.LENGTH_SHORT)
+                    .show()
+            },
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-        // Creating a button that on
-        // click displays/shows the DatePickerDialog
-        Button(onClick = {
-            mDatePickerDialog.show()
-        }, colors = ButtonDefaults.buttonColors(backgroundColor = Color(0XFF0F9D58))) {
-            Text(text = "Open Date Picker", color = Color.White)
-        }
-
-        // Adding a space of 100dp height
-        Divider(color = Color.Blue, thickness = 1.dp)
-
-        // Displaying the mDate value in the Text
-        Text(text = "Selected Date: ${mDate.value}", fontSize = 30.sp, textAlign = TextAlign.Center)
+        Text(
+            text = "Specific Date",
+            color = dimColor(SpacePrimaryVariant, disable)
+        )
+        date.value = datePicker(buttonModifier = Modifier, buttonEnabled = !disable)
     }
+
+    return date.value
+}
+
+
+@Composable
+fun dateRangePickerWidget(disable: Boolean): Pair<String, String> {
+    val startDate = remember { mutableStateOf("") }
+    val endDate = remember { mutableStateOf("") }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "Interval",
+            color = dimColor(SpacePrimaryVariant, disable)
+        )
+        Box(modifier = Modifier.fillMaxWidth(0.7f)) {
+            startDate.value =
+                datePicker(buttonModifier = Modifier.align(Alignment.CenterStart), !disable)
+            Divider(
+                color = dimColor(SpacePrimaryVariant, disable),
+                thickness = 1.dp,
+                modifier = Modifier
+                    .fillMaxWidth(0.2f)
+                    .padding(16.dp, 0.dp)
+                    .align(Alignment.Center)
+            )
+            endDate.value =
+                datePicker(buttonModifier = Modifier.align(Alignment.CenterEnd), !disable)
+        }
+    }
+
+    return Pair(startDate.value, endDate.value)
 }
 
 @Composable
-fun datePicker(): Date {
-    return Date()
+fun datePicker(buttonModifier: Modifier, buttonEnabled: Boolean): String {
+    val context = LocalContext.current
+    val year: Int
+    val month: Int
+    val day: Int
+    val calendar = Calendar.getInstance()
+    val buttonLabel = remember { mutableStateOf("Select") }
+
+    year = calendar.get(Calendar.YEAR)
+    month = calendar.get(Calendar.MONTH)
+    day = calendar.get(Calendar.DAY_OF_MONTH)
+
+    calendar.time = Date()
+
+    val date = remember { mutableStateOf("") }
+
+    val datePickerDialog = DatePickerDialog(
+        context,
+        { _: DatePicker, pickerYear: Int, pickerMonth: Int, pickerDay: Int ->
+            date.value = "$pickerYear-${pickerMonth + 1}-$pickerDay"
+        },
+        year,
+        month,
+        day
+    )
+    datePickerDialog.setOnDateSetListener { _, yyyy, mm, dd ->
+        date.value = "$yyyy-$mm-$dd"
+        buttonLabel.value = date.value
+    }
+
+    datePickerDialog.setOnCancelListener {
+        // TODO: make string static or const
+        date.value = "cancelled"
+        buttonLabel.value = date.value
+    }
+    datePickerDialog.datePicker.maxDate = calendar.time.time
+
+    OutlinedButton(
+        enabled = buttonEnabled,
+        onClick = {
+            datePickerDialog.show()
+        },
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = MaterialTheme.colors.primaryVariant,
+            contentColor = MaterialTheme.colors.primary,
+            disabledBackgroundColor = MaterialTheme.colors.primaryVariant.copy(.0f),
+            disabledContentColor = MaterialTheme.colors.error.copy(.7f),
+        ),
+        modifier = buttonModifier
+    ) {
+        Text(text = buttonLabel.value, color = dimColor(DimmedWhite, !buttonEnabled))
+    }
+    return date.value
+}
+
+@Composable
+fun SearchButton() {
+
+    Button(
+        shape = RoundedCornerShape(8.dp),
+        onClick = { /*TODO*/ },
+        colors = ButtonDefaults.buttonColors(backgroundColor = SpaceSecondary)
+    ) {
+        Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+    }
+}
+
+fun dimColor(color: Color, disable: Boolean): Color {
+    //make use of default
+    return if (disable) {
+        color.copy(0.6f)
+    } else {
+        color
+    }
 }
