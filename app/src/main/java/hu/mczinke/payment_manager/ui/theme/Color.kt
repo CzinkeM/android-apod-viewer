4ff3348abb6469d328bc116de2a32821a61856a2
CzinkeM <mczinke@protonmail.com>
2022-07-27 16:28:53 +0200

    Added a dimmed white color.

--- app/src/main/java/hu/mczinke/payment_manager/ui/theme/Color.kt
+++ app/src/main/java/hu/mczinke/payment_manager/ui/theme/Color.kt
@@ -7,3 +7,5 @@ val SpaceBlackVariant = Color(0xFF252525)
 val SpacePrimary = Color(0xFF2C5484)
 val SpacePrimaryVariant = Color(0xFF80A7C4)
 val SpaceSecondary = Color(0xFFCDCBDA)
+
+val DimmedWhite = Color(0xFFF7F7F7)
