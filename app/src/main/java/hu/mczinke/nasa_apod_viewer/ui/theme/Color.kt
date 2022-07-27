94c12e2e9571f994f7cd18480766e27eb44ff698
CzinkeM <mczinke@protonmail.com>
2022-07-27 19:23:28 +0200

    Added a vibrant color.

--- app/src/main/java/hu/mczinke/nasa_apod_viewer/ui/theme/Color.kt
+++ app/src/main/java/hu/mczinke/nasa_apod_viewer/ui/theme/Color.kt
@@ -7,5 +7,6 @@ val SpaceBlackVariant = Color(0xFF252525)
 val SpacePrimary = Color(0xFF2C5484)
 val SpacePrimaryVariant = Color(0xFF80A7C4)
 val SpaceSecondary = Color(0xFFCDCBDA)
+val VibrantColor = Color(0xFFFC852A)
 
 val DimmedWhite = Color(0xFFF7F7F7)
