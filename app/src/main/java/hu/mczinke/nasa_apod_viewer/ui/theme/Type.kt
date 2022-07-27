7ce79162b28a5886546a3dabeaacfab53af0b626
CzinkeM <mczinke@protonmail.com>
2022-07-27 19:24:01 +0200

    Made use of custom font.

--- app/src/main/java/hu/mczinke/nasa_apod_viewer/ui/theme/Type.kt
+++ app/src/main/java/hu/mczinke/nasa_apod_viewer/ui/theme/Type.kt
@@ -2,17 +2,32 @@ package hu.mczinke.nasa_apod_viewer.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import hu.mczinke.nasa_apod_viewer.R
 
 // Set of Material typography styles to start with
+val RobotoCondensed = FontFamily(
+        Font(R.font.roboto_condensed_normal, FontWeight.Normal, FontStyle.Normal),
+        Font(R.font.roboto_condensed_bold, FontWeight.Bold, FontStyle.Normal),
+        Font(R.font.roboto_condensed_italic, FontWeight.Normal, FontStyle.Italic),
+        Font(R.font.roboto_condensed_light, FontWeight.Light, FontStyle.Normal),
+)
+
 val Typography = Typography(
         body1 = TextStyle(
-                fontFamily = FontFamily.Default,
+                fontFamily = RobotoCondensed,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp
-        )
+        ),
+        button = TextStyle(
+                fontFamily = RobotoCondensed,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp
+        ),
         /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
