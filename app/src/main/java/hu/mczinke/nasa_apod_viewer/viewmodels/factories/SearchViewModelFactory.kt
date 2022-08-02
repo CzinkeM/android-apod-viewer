ef4c087eec873cb23f6bd4145a2a7f4c917dc9e8
CzinkeM <mczinke@protonmail.com>
2022-08-02 11:37:14 +0200

    Moved viewmodel factories to another package.

--- app/src/main/java/hu/mczinke/nasa_apod_viewer/viewmodels/factories/SearchViewModelFactory.kt
+++ app/src/main/java/hu/mczinke/nasa_apod_viewer/viewmodels/factories/SearchViewModelFactory.kt
@@ -1,7 +1,9 @@
-package hu.mczinke.nasa_apod_viewer.viewmodels
+package hu.mczinke.nasa_apod_viewer.viewmodels.factories
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import hu.mczinke.nasa_apod_viewer.viewmodels.Repository
+import hu.mczinke.nasa_apod_viewer.viewmodels.SearchViewModel
 
 
 class SearchViewModelFactory(private val repository: Repository) : ViewModelProvider.Factory {
