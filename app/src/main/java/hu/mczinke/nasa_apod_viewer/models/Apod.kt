36fafea673814bc4661655781d3a0443aee209bd
CzinkeM <mczinke@protonmail.com>
2022-08-04 09:14:18 +0200

    Added ToEntity method.

--- app/src/main/java/hu/mczinke/nasa_apod_viewer/models/Apod.kt
+++ app/src/main/java/hu/mczinke/nasa_apod_viewer/models/Apod.kt
@@ -1,5 +1,7 @@
 package hu.mczinke.nasa_apod_viewer.models
 
+import hu.mczinke.nasa_apod_viewer.models.entities.ApodEntity
+
 
 data class Apod(
     val copyright: String,
@@ -11,6 +13,10 @@ data class Apod(
 ) {
     override fun toString(): String {
         return "$copyright: $title($date)"
+    }
+
+    fun toEntity(): ApodEntity {
+        return ApodEntity(0, copyright, date, explanation, HDUrl, title, url)
     }
 
     companion object StaticApods {
