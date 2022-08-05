07dba7be85f25aa44a170f6ccbffc6e0fc3e9e39
CzinkeM <mczinke@protonmail.com>
2022-08-05 14:55:59 +0200

    Improved apod card.

--- app/src/main/java/hu/mczinke/nasa_apod_viewer/ui/components/ApodCard.kt
+++ app/src/main/java/hu/mczinke/nasa_apod_viewer/ui/components/ApodCard.kt
@@ -1,41 +1,45 @@
 package hu.mczinke.nasa_apod_viewer.ui.components
 
-import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
-import com.skydoves.landscapist.CircularReveal
-import com.skydoves.landscapist.glide.GlideImage
+import hu.mczinke.nasa_apod_viewer.R
 import hu.mczinke.nasa_apod_viewer.models.Apod
-import hu.mczinke.nasa_apod_viewer.ui.preview_parameter_providers.ApodParameterProvider
 import hu.mczinke.nasa_apod_viewer.ui.theme.SpaceBlackVariant
+import hu.mczinke.nasa_apod_viewer.viewmodels.DatabaseRelatedViewModel
 
 @Composable
-@Preview
-fun ApodCard(@PreviewParameter(ApodParameterProvider::class) apod: Apod) {
+fun ApodCard(
+    apod: Apod,
+    isDelete: Boolean,
+    viewModel: DatabaseRelatedViewModel,
+    modifier: Modifier = Modifier
+) {
     val context = LocalContext.current
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
             .padding(8.dp)
-            .clickable {
-                Toast
-                    .makeText(context, "clicked", Toast.LENGTH_SHORT)
-                    .show()
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onDoubleTap = {
+                        viewModel.addApodToDatabase(apod)
+                    }
+                )
             },
         backgroundColor = SpaceBlackVariant,
         elevation = 4.dp,
@@ -47,25 +51,47 @@ fun ApodCard(@PreviewParameter(ApodParameterProvider::class) apod: Apod) {
                 .fillMaxHeight()
                 .padding(8.dp)
         ) {
-            GlideImage(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(8.dp))
-                    .weight(1f),
-                imageModel = apod.url,
-                contentDescription = "Picture named: ${apod.title}",
-                contentScale = ContentScale.FillHeight,
-                placeHolder = ImageVector.vectorResource(id = hu.mczinke.nasa_apod_viewer.R.drawable.ic_image),
-                error = ImageVector.vectorResource(id = hu.mczinke.nasa_apod_viewer.R.drawable.ic_image),
-                previewPlaceholder = hu.mczinke.nasa_apod_viewer.R.drawable.ic_image,
-                circularReveal = CircularReveal(150)
-
-            )
-            Text(
-                modifier = Modifier
-                    .weight(2f),
-                text = apod.title,
-                textAlign = TextAlign.Center
-            )
+            ApodImage(apod = apod, useHD = false, modifier = Modifier.weight(1f))
+            Box(
+                modifier = modifier
+                    .fillMaxSize()
+                    .weight(2f)
+            ) {
+                Text(
+                    text = apod.title,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.TopCenter)
+                )
+                if (isDelete) {
+                    DeleteIcon(
+                        apod = apod,
+                        viewModel = viewModel,
+                        modifier = Modifier.align(
+                            Alignment.BottomEnd
+                        )
+                    )
+                } else {
+                    FavoriteIcon(
+                        apod = apod,
+                        viewModel = viewModel,
+                        modifier = Modifier.align(
+                            Alignment.BottomEnd
+                        )
+                    )
+                }
+            }
         }
     }
+}
+
+@Composable
+fun DeleteIcon(apod: Apod, viewModel: DatabaseRelatedViewModel, modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier
+            .clickable {
+                viewModel.deleteApodFromDatabase(apod)
+            },
+        imageVector = ImageVector.vectorResource(id = R.drawable.ic_remove),
+        contentDescription = "Thrash can image."
+    )
 }
