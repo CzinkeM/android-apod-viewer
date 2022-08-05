1dc789a1c5153ca802869e02092907badd7eb826
CzinkeM <mczinke@protonmail.com>
2022-08-05 15:02:45 +0200

    Extended the DAO interface with queries.

--- app/src/main/java/hu/mczinke/nasa_apod_viewer/datamanagement/ApodDao.kt
+++ app/src/main/java/hu/mczinke/nasa_apod_viewer/datamanagement/ApodDao.kt
@@ -6,12 +6,20 @@ import hu.mczinke.nasa_apod_viewer.models.entities.ApodEntity
 
 @Dao
 interface ApodDao {
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addApod(apod: ApodEntity)
 
     @Delete
     suspend fun removeApod(apod: ApodEntity)
 
+    @Query("DELETE FROM favorite_apods WHERE title LIKE :apodTitle")
+    suspend fun removeApodByTitle(apodTitle: String)
+
     @Query("SELECT * FROM favorite_apods")
     fun allApod(): LiveData<List<ApodEntity>>
+
+    @Query("SELECT * FROM favorite_apods WHERE favorite_apods.title LIKE :apodTitle")
+    fun isApodExist(apodTitle: String): LiveData<ApodEntity?>
+
+
 }
